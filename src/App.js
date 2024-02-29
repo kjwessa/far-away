a28fa89d724c43d3fce231fbd3fb6d8268a3404c
Kevin Wessa <kjwessa@gmail.com>
2024-02-29 11:15:28 -0600

    Added initial Form structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ export default function App() {}
 function Logo() {
   return <h1>🏝️ Far Away 👜</h1>;
 }
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip? </h3>
+    </div>
+  );
+}
 function PackingList() {}
 function Stats() {}
