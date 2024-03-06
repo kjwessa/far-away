3feab57847bfa2aae1be436550c80850fa7cc6e6
Kevin Wessa <kjwessa@gmail.com>
2024-03-06 09:51:30 -0600

    Added derived state to calculate packing stats in the footer

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -90,10 +90,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list.</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {numPacked === numItems
+          ? "You got everything! Ready to go 😀"
+          : ` 👜 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%) items.`}
+      </em>
     </footer>
   );
 }
