e96912b90931c6bd47436ac3def4b4e622a5b720
Kevin Wessa <kjwessa@gmail.com>
2024-03-07 10:30:06 -0600

    Refactored components into separate files

---
+++ src/components/Stats.js
@@ -0,0 +1,22 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list.</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {numPacked === numItems
+          ? "You got everything! Ready to go 😀"
+          : ` 👜 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%) items.`}
+      </em>
+    </footer>
+  );
+}
